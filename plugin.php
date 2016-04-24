3db6d51cb594bf074dfcde878505819421965902
Taras Mankovski <tarasm@gmail.com>
2016-04-24 19:14:47 -0400

    Added GitHub Plugin URI to header

--- plugin.php
+++ plugin.php
@@ -2,6 +2,7 @@
 /*
 Plugin Name: WP-REST JSONAPI Serializer
 Plugin URI: http://github.com/EmberSherpa/wp-rest-jsonapi-serializer
+GitHub Plugin URI: EmberSherpa/wp-rest-jsonapi-serializer
 Description: Converts WP REST API Response to JSONAPI spec
 Version: 1.0
 Author: Taras Mankovski
