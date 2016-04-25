c868e4d4ac12feef3e560354b336b2f86b5b77a4
Taras Mankovski <tarasm@gmail.com>
2016-04-24 21:50:16 -0400

    Running rest_pre_serve_request after headers are setn

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ Plugin Name: WP-REST JSONAPI Serializer
 Plugin URI: http://github.com/EmberSherpa/wp-rest-jsonapi-serializer
 GitHub Plugin URI: EmberSherpa/wp-rest-jsonapi-serializer
 Description: Converts WP REST API Response to JSONAPI spec
-Version: 1.0.1
+Version: 1.0.2
 Author: Taras Mankovski
 Author URI: http://www.embersherpa.com
 License: GPL2
@@ -17,9 +17,8 @@ class WP_REST_JSOINAPI_Serializer {
 
     static function initialize() {
         add_filter('rest_post_dispatch', 'JSONAPI_Serializer::rest_post_dispatch', 10, 4);
-        add_filter('rest_pre_serve_request', 'JSONAPI_Serializer::rest_pre_serve_request', 10, 4);
+        add_filter('rest_pre_serve_request', 'JSONAPI_Serializer::rest_pre_serve_request', 15, 4);
     }
-
 }
 
 add_action('init', 'WP_REST_JSOINAPI_Serializer::initialize');
