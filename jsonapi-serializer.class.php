6d3ffb343c333cb45b367f843dd6afe08ccd1879
Taras Mankovski <tarasm@gmail.com>
2016-04-27 17:32:37 -0400

    Made total pages dasherized

--- jsonapi-serializer.class.php
+++ jsonapi-serializer.class.php
@@ -28,7 +28,7 @@ class JSONAPI_Serializer {
 
                 $meta = array(
                     'total' => $result->headers['X-WP-Total'],
-                    'totalPages' => $result->headers['X-WP-TotalPages'],
+                    'total-pages' => $result->headers['X-WP-TotalPages'],
                     'page' => get_query_var('page') ? get_query_var('page') : 1
                 );
 
